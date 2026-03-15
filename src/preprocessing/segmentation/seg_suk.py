81af459bb4e1a9f57871ce5dfa622e4057be4276
Oliver Morgan <oliver.parker.morgan@gmail.com>
2026-03-15 15:43:10 +0100

    Refactor code structure for improved readability and maintainability

--- src/preprocessing/segmentation/seg_suk.py
+++ src/preprocessing/segmentation/seg_suk.py
@@ -1,5 +1,21 @@
-from .seg_common import segment_using_superpixels_and_kmeans
+from .seg_common import segment_using_superpixels_and_kmeans, has_outliers
+import numpy as np
 
 
-def segment_suk(img, k=16, attempts=3, seed=42):
-   return segment_using_superpixels_and_kmeans(img, k, attempts, seed, [k-1])
+SUK_SUPERPIXEL_REGION_SIZE = 12
+SUK_SUPERPIXEL_RULER = 1.0
+
+
+def segment_suk(img, k=20, attempts=1, seed=42):
+   if has_outliers(img):
+      return segment_using_superpixels_and_kmeans(
+         img,
+         k,
+         attempts,
+         seed,
+         [k - 1],
+         region_size=SUK_SUPERPIXEL_REGION_SIZE,
+         ruler=SUK_SUPERPIXEL_RULER,
+      )
+   
+   return np.zeros(img.shape[:2], dtype=np.uint8)
